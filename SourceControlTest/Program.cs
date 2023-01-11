9fee570dc4183d7bd92d8ba3e9330934fdac3d79
Zcooke1 <115667096+Zcooke1@users.noreply.github.com>
2023-01-10 22:11:51 -0800

    added final writeline in console.

--- SourceControlTest/Program.cs
+++ SourceControlTest/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("This is a test for the first commit.");
 Console.WriteLine("This is just a test for the second commit.");
 Console.WriteLine("11 + 22 * 3 = " + 77 + "!");
 Console.WriteLine("Is this the 2nd to last commit or only the last one?");
+Console.WriteLine("Okay, this one for sure.");
